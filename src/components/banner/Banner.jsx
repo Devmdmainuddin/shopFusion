da75dc3986fe437c781881831a4f5505e3e3c8c7
Devmdmainuddin <devmdmainuddin@gmail.com>
2024-07-09 01:27:41 +0600

    card design

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,26 @@
+import Container from "../layer/Container";
+import banner_1 from '/banner-1.jpg'
+import banner_2 from '/banner-2.jpg'
+import banner_3 from '/banner-3.jpg'
+const Banner = () => {
+    return (
+        <div className="my-[128px]">
+            <Container>
+                <div className="flex justify-between flex-col md:flex-row gap-6">
+                    <div>
+                        {/* className=" w-full h-auto md:w-[780] md:h-[780px] " */}
+                        <img src={banner_1} alt="" className=" w-full h-auto md:max-w-[780] md:max-h-[780px] " />
+                    </div>
+                    <div className="flex flex-col  gap-6">
+                        {/* className="w-full h-auto md:w-[784] md:h-[370px] " */}
+                    <img src={banner_2} alt=""  className="w-full h-auto md:max-w-[784] md:max-h-[370px] "  />
+                    <img src={banner_3} alt="" className="w-full h-auto md:max-w-[784] md:max-h-[370px] " />
+                    </div>
+
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default Banner;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,14 @@
+import Banner from "../components/banner/Banner";
 import HerroBanner from "../components/banner/HerroBanner";
+import Card from "../components/card/Card";
 import Searchbar from '../components/share/Searchbar'
 const Home = () => {
     return (
         <div>
             <Searchbar></Searchbar>
            <HerroBanner></HerroBanner>
+           <Banner></Banner>
+           <Card></Card>
         </div>
     );
 };
